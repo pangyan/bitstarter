9da2f7149f90992423ed7e1f9ce60c73fa8f1cd9
Ubuntu <ubuntu@ip-172-31-44-184.us-west-2.compute.internal>
2013-07-27 05:06:51 +0000

    ::

---
+++ web.20130726.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  var body = fs.readFileSync('./index.html', 'utf-8');
+  response.send(body);
+  // response.send('Hello World 2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
