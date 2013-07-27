2e26f224b132fe7613d572dc519363e2d055e6a7
Ubuntu <ubuntu@ip-172-31-44-184.us-west-2.compute.internal>
2013-07-27 05:02:42 +0000

    git add checks.json git add checks.orig.json git add grader.js git add grader.orig.js git add hw3p3.txt git add index.part1.html git add web.20130726.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
-var app = express();
-app.use(express.logger());
+var app = express.createServer(express.logger());
+// app.use(express.logger());
 
 app.get('/', function(request, response) {
   var body = fs.readFileSync('./index.html', 'utf-8');
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   // response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
